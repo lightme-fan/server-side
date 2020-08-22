706dbf22e9205c57ca0b4f89e575c5fd4a6c8f56
Fanilo Andriamarohaja <fanilo.and@onja.org>
2020-08-22 21:12:26 +0100

    Added upstream main

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Greetings from Node and index.js! 🙃');
+// We need Express to act as our application server
+const express = require('express');
+
+// Let's test that Express is working as expected:
+console.log(express);
+
+// Let's create a new App
+const app = express();
+
+// We want express to run on this port
+const port = 3000;
+
+// Add this before your app.listen() declaration
+app.get('/', (request, response) => {
+  console.log('Request: /');
+  response.sendFile(__dirname + '/index.html');
+});
+
+// Listen for app
+app.listen(port, () => {
+  console.log(`app is running here: http://localhost:${port}`);
+})
