7e78f6dadf0dd8a20d7dcf97194fab575136f4ec
Fanilo Andriamarohaja <fanilo.and@onja.org>
2020-08-25 09:37:32 +0100

    Added html.njk and installed nodemon

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const nunjucks = require('nunjucks');
+
 // We need Express to act as our application server
 const express = require('express');
 
@@ -7,13 +9,17 @@ console.log(express);
 // Let's create a new App
 const app = express();
 
+nunjucks.configure({
+  autoescape: true,
+  express: app
+});
+
 // We want express to run on this port
 const port = 3000;
 
 // Add this before your app.listen() declaration
 app.get('/', (request, response) => {
-  console.log('Request: /');
-  response.sendFile(__dirname + '/index.html');
+  response.render('./views/index.html.njk');
 });
 
 // Listen for app
